c7ce742b9a08cbbeaf84013b2e2a73a63eebbe7e
LimaevAV <Limaevav@gmail.com>
2018-06-25 11:30:32 +0300

    Initial commit

---
+++ Poker.ChartsViewer/ViewModel/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace Poker.Charts.ViewModel
+{
+    class MainViewModel : ViewModelBase
+    {
+        public ChartsManagerViewModel LeftChartsManager { get; set; } = new ChartsManagerViewModel();
+
+        public ChartsManagerViewModel RightChartsManager { get; set; } = new ChartsManagerViewModel();
+    }
+}
